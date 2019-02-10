9fad8b671f7a6160e2c457b4de50ac9b01ce1440
Ru Nacken <ru@chunk.nl>
2019-02-10 18:05:33 +0100

    Updates

---
+++ precache-manifest.edcf032c850c3f6730ec0a2473dbadfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "48559fed6f87ebb56bcb",
+    "url": "/static/js/main.48559fed.chunk.js"
+  },
+  {
+    "revision": "bfac175ae2356c68ace6",
+    "url": "/static/js/1.bfac175a.chunk.js"
+  },
+  {
+    "revision": "bfac175ae2356c68ace6",
+    "url": "/static/css/1.6109899d.chunk.css"
+  },
+  {
+    "revision": "f39e9ab721e293badd1f9146a5389a22",
+    "url": "/index.html"
+  }
+];
